5cf07f05e9613547086802415dcf2b7ae2445926
Joshua Freeman <github@frejos.net>
2022-03-05 20:45:50 -0600

    remove un-needed locale support to decrease size

--- lambda/smarthome/alexa/v3/config.js
+++ lambda/smarthome/alexa/v3/config.js
@@ -564,8 +564,7 @@ module.exports = Object.freeze({
    * @type {Array}
    */
   LOCALES: [
-    'de-DE', 'en-AU', 'en-CA', 'en-GB', 'en-IN', 'en-US', 'es-ES', 'es-MX', 'es-US',
-    'fr-CA', 'fr-FR', 'hi-IN', 'it-IT', 'ja-JP', 'pt-BR'
+    'en-US'
   ],
 
   /**
